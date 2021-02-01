f3db7d98e94417c35293de94e665bef56384c514
KARTIKNISHAD204 <36085008+KARTIKNISHAD204@users.noreply.github.com>
2021-02-02 04:59:55 +0530

    first commit

---
+++ constants/dummyToDoList.js
@@ -0,0 +1,6 @@
+export const todoItems = [
+    { key: Math.random().toString(), data: "React Native Basics" },
+    { key: Math.random().toString(), data: "React Native Components" },
+    { key: Math.random().toString(), data: "How its Different" },
+
+]
